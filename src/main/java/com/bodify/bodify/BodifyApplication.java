6b8161789ed2b31167ff7595d6d50ef52e03eb31
Mario <grebelskimario@gmail.com>
2020-05-10 14:34:59 +0100

    Initial commit

---
+++ src/main/java/com/bodify/bodify/BodifyApplication.java
@@ -0,0 +1,13 @@
+package com.bodify.bodify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BodifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bodify/bodify/BodifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bodify.bodify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
